4e3136332a11d05558a53d89263661360fed5cc1
ikolodiazhnyi <itsfunnytoseeyouhere@gmail.com>
2021-07-08 18:08:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kflqj4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kflqj4)
---
+++ src/app/products-alerts/products-alerts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Product } from '../products';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-products-alerts',
+  templateUrl: './products-alerts.component.html',
+  styleUrls: ['./products-alerts.component.css']
+})
+export class ProductsAlertsComponent implements OnInit {
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
